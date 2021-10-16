4b25a97957f3466ece5f3dae2337f05fca06348c
dheksaaryadityar <85921289+dheksaaryadityar@users.noreply.github.com>
2021-10-16 20:14:12 +0700

    Add files via upload

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Dheksa Aryaditya Ramadhan,
+            Saya tinggal di Bekasi Timur,
+            Saya suka menongston film dan sedikit bermain gimang";  
+    }
+}
